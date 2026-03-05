983ecb575756160b59adc3cd23157d22df43cf25
Francisco Gomez Palomo <xisco1998@gmail.com>
2026-03-05 14:00:19 +0100

    feat: implement API logic and endpoints for sections 2.2 and 2.3

---
+++ src/db.ts
@@ -0,0 +1,10 @@
+import { Pool } from "postgres"; 
+import { load } from "@std/dotenv"; 
+await load({ export: true }); 
+export const pool = new Pool({ 
+hostname: Deno.env.get('DB_HOST') ?? 'localhost', 
+database: Deno.env.get('DB_NAME') ?? 'cologne_datahub', 
+user: Deno.env.get('DB_USER') ?? 'postgres', 
+password: Deno.env.get('DB_PASSWORD') ?? 'Usuario1+',
+port: Number(Deno.env.get('DB_PORT') ?? 5432), 
+}, 10); 
