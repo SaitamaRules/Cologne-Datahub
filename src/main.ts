f1adfbba24ffc548c589e7c2ab28c2d70253b922
Francisco Gómez Palomo <xisco1998@gmail.com>
2026-03-25 13:12:47 +0100

    docs: added OpenAPI documentation and swagger HTML page

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import { Hono } from "hono";
 import { apiRouter } from "./routes/arboles.ts";
 import { mongoRouter } from "./routes/arboles_mongo.ts";
+import { serveStatic } from "hono/deno";
 
 const app = new Hono();
 
 // montar todas las rutas bajo el prefijo /api
 app.route("/api", apiRouter);
 app.route("/api/mongo", mongoRouter);
+
+app.get("/docs/openapi.json", serveStatic({ path: "./docs/openapi.json" }));
+app.get("/docs", serveStatic({ path: "./docs/swagger.html" }));
 
 Deno.serve({ port: 8000 }, app.fetch);
